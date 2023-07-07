b94bd19aa61edbfb32531ecfca37fde8be2b4ac9
Ruben Carlo Benante <rcb@beco.cc>
2023-07-07 17:33:34 -0300

    added myeulerlib

---
+++ myeulerlib.c
@@ -0,0 +1,58 @@
+/***************************************************************************
+ *   myeulerlib.c                             Version 20190817.225830      *
+ *                                                                         *
+ *   My own Proj Euler Lib Functions                                       *
+ *   Copyright (C) 2019         by Ruben Carlo Benante                     *
+ ***************************************************************************
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; version 2 of the License.               *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************
+ *   To contact the author, please write to:                               *
+ *   Ruben Carlo Benante                                                   *
+ *   Email: rcb@beco.cc                                                    *
+ *   Webpage: http://www.beco.cc                                           *
+ *   Phone: +55 (81) 3184-7555                                             *
+ ***************************************************************************/
+
+/* add your functions in the end of this file */
+
+/* ---------------------------------------------------------------------- */
+/* includes */
+#include "myeulerlib.h"
+
+/* ---------------------------------------------------------------------- */
+/* Prints help information 
+ *  usually called by opt -h or --help
+ */
+void help(void)
+{
+    IFDEBUG("help()");
+    printf("%s - %s\n", "myeulerlib", "My own Proj Euler Lib Functions");
+    printf("\nUsage: %s\n\n", "./peN.x");
+    printf("This program calculates Project Euler problem number N\n");
+    printf("To print a parcial calculation, use CTRL-C\n");
+    /* add more stuff here */
+    printf("\nExit status:\n\t0 if ok.\n\t1 some error occurred.\n");
+    printf("\nTodo:\n\tLong options not implemented yet.\n");
+    printf("\nAuthor:\n\tWritten by %s <%s>\n\n", "Ruben Carlo Benante", "rcb@beco.cc");
+    return;
+}
+
+/* ---------------------------------------------------------------------- */
+/* add more functions here */
+
+/* ---------------------------------------------------------------------- */
+/* vi: set ai et ts=4 sw=4 tw=0 wm=0 fo=croql : C config for Vim modeline */
+/* Template by Dr. Beco <rcb at beco dot cc> Version 20160612.142044      */
+
---
+++ myeulerlib.h
@@ -0,0 +1,50 @@
+/***************************************************************************
+ *   myeulerlib.h                             Version 20190817.225147      *
+ *                                                                         *
+ *   My own Proj Euler Lib                                                 *
+ *   Copyright (C) 2019         by Ruben Carlo Benante                     *
+ ***************************************************************************
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License.        *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************
+ *   To contact the author, please write to:                               *
+ *   Ruben Carlo Benante                                                   *
+ *   Email: rcb@beco.cc                                                    *
+ *   Webpage: www.beco.cc                                                  *
+ *   Phone: +55 (81) 3184-7555                                             *
+ ***************************************************************************/
+
+/* add your library prototypes in the end of this file */
+
+/* ---------------------------------------------------------------------- */
+/* include guard */
+#ifndef _MYEULERLIB_H
+#define _MYEULERLIB_H
+
+/* ---------------------------------------------------------------------- */
+/* includes */
+#include "projecteuler.h"
+
+/* ---------------------------------------------------------------------- */
+/* prototypes */
+
+void help(void); /**< Prints help information and exit */
+/* add more prototypes here */
+
+#endif /* NOT def _MYEULERLIB_H */
+
+/* ---------------------------------------------------------------------- */
+/* vi: set ai et ts=4 sw=4 tw=0 wm=0 fo=croql : C config for Vim modeline */
+/* Template by Dr. Beco <rcb at beco dot cc> Version 20160615.020326      */
+
